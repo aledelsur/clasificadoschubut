2a97e1fec02ed84b3c08a42c020701d42b320a22
Ale - aledelsur <alealvarez_00@hotmail.com>
2013-03-21 09:16:12 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Clasificadoschubut::Application.config.secret_token = '77848d4dcdefc527b286fc91cf84edf4bedcd65255027fbc487a2a1ec08585cfe01976b67baa492b17edd34dd297830bccb8d27b06afe74c289efd41dac8bfb7'
